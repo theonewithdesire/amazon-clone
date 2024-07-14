7a798629452bdd2ba5dc0965be899278015ffe30
ahd <a.dadjoo55@gmail.com>
2024-07-14 12:01:57 +0330

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,15 +3,12 @@ import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions ,getDeliveryOption} from '../../data/deliveryOptions.js'
+import {deliveryOptions ,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 
-hello();
 
-const today = dayjs(); 
-const delivaryDate  = today.add(7,'days');
-console.log(delivaryDate.format('dddd, MMMM D'));
 
 
 
@@ -123,6 +120,7 @@ export function renderOrderSummary(){
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+      renderPaymentSummary();
     });
   });
 
@@ -131,6 +129,7 @@ export function renderOrderSummary(){
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
 
 
     });

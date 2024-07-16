8e66e037ca1b7d7c43903fde3d249cdf2d6ac96a
ahd <a.dadjoo55@gmail.com>
2024-07-16 23:27:51 +0330

    use inheritence to add size chart

--- data/products.js
+++ data/products.js
@@ -34,25 +34,29 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`;
 
   }
+  extraInfoHTML(){
+    return ''; 
+  }
 }
 
-const product1 = new Product(
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
+
+
+
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+    
   }
-);
+  extraInfoHTML(){
+    return `<a href="${this.sizeChartLink}" target="_blank"> size chart </a>`
+  }
+
+}
+
 
 
 
@@ -719,6 +723,10 @@ const product1 = new Product(
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+
+  }
   return new Product(productDetails);
 
 });

8086351f5caf84d50dbeea5119c3ee436181d18b
ahd <a.dadjoo55@gmail.com>
2024-07-18 11:22:23 +0330

    learn more details about this

--- data/products.js
+++ data/products.js
@@ -59,6 +59,26 @@ class Clothing extends Product{
 
 
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+
+function logThis(){
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+const object3 = {
+  method : () => {
+    console.log(this)
+  }
+}
+object3.method();
+*/
+
+
 
 
 

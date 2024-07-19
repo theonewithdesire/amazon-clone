ad32d5659cd84a7dfb878cfe37ffaadc94ffb93b
ahd <a.dadjoo55@gmail.com>
2024-07-19 14:35:31 +0330

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -78,3 +78,19 @@ export function updateDeliveryOption(productId, deliveryOptionId){
 
 
 }
+
+
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response)
+    fun();
+  })
+
+
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,69 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+
+
+Promise.all([
+    new Promise((reslove) => {
+        loadProducts(() => {
+         reslove('value1');
+        });
+     }),
+    new Promise((reslove) => {
+        loadCart(() => {
+         reslove();
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
+});
+
+
+
+
+
+
+
+/*
+new Promise((reslove) => {
+   loadProducts(() => {
+    reslove('value1');
+   });
+
+}).then((value) => {
+    console.log(value);
+    return new Promise((reslove) => {
+        loadCart(() => {
+         reslove();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+    
 })
+*/
+
